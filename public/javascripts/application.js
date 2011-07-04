7b03f8f7f6998f05f43993536d0552c1a325c4b7
Amiel Martin <amiel@carnesmedia.com>
2011-07-04 15:20:11 -0700

    somewhat reasonable protoype

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
     $('.pomodoros .start.time').each(function(index) {
+        var start_element = $(this),
+            start_time = start_element.attr('title'),
+            start_date = new Date(Date.parse(start_time)),
+            end_date = new Date(+start_date + (25 * 60 * 1000)),
+            left_element = start_element.parent().find('.left');
+
+        // console.log(start_element, start_time, start_date, end_date);
+
+        setInterval(function() {
+            var now = new Date,
+                seconds_left = (end_date - now) / 1000,
+                minutes_left = Math.floor(seconds_left / 60),
+                minute_seconds_left = Math.floor(seconds_left % 60);
+            left_element.text(minutes_left + ":" + minute_seconds_left);
+        }, 1000);
 
     });
 });
